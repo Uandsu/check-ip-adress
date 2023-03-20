3cc83d5d8a60d59dce4ad5312303670a1e3141e3
Uandsu <uandersonjr@gmail.com>
2023-03-20 16:53:32 -0300

    find servers

--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Generate() *cli.App {
 	app.Name = "Application Command Line"
 	app.Usage = "Find Ips and Internet Server Names"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Find IPS",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Find IPS",
+			Flags:  flags,
 			Action: findIps,
+		},
+		{
+			Name:   "Servers",
+			Usage:  "Find Internet Server Names",
+			Flags:  flags,
+			Action: findServers,
 		},
 	}
 
@@ -42,5 +50,18 @@ func findIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func findServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host) // name server
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server)
 	}
 }
