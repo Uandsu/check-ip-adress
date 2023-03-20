b7a622d763bbee3f99de1f85da4087c99ead4768
Uandsu <uandersonjr@gmail.com>
2023-03-20 16:07:54 -0300

    add find ips

--- app/app.go
+++ app/app.go
@@ -1,13 +1,46 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-//Gerar vai retornar a aplicação
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Application Command Line"
 	app.Usage = "Find Ips and Internet Server Names"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Find IPS",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: findIps,
+		},
+	}
+
 	return app
 
+}
+
+func findIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
